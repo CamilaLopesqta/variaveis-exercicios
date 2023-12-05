5e9b9ebca1ad7d791e34f8855eb63a7fbd42910c
Camila Lopes <camilalopesqta@gmail.com>
2023-12-05 13:25:20 -0300

    exercícios de variaveis

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,8 +1,32 @@
-// declarar uma variavel para armazenar no e uma para armazenar idade sem valores
+/* ## Parte 1
+Construa um programa, seguindo as instruções abaixo:
 
-//let nome1
-//let idade1
-//console.log(typeof nome1, "e", typeof idade1)
+**a)** Declare uma variável para armazenar um **nome**, sem atribuir um valor a essa variável.
+
+**b)** Declare uma variável para armazenar uma **idade**, sem atribuir um valor a essa variável.
+
+**c)** Imprima na tela o **tipo** dessas variáveis que acabou de criar, usando o comando `typeof`.
+
+**d)** Reflita: por que esse tipo foi impresso? Escreva a resposta em um comentário de código.
+
+**e)** Depois, disso, atualize o código para **perguntar** ao usuário seu nome e sua idade, atribuindo esses dois valores às variáveis que acabou de criar.
+    
+
+>💡Se você receber um erro de código nessa etapa, reflita sobre o tipo de declaração que utilizou.
+    
+
+**f)** Novamente, imprima na tela o tipo dessas variáveis. O que você notou? Escreva em um comentário de código.
+
+**g)** Para finalizar, imprima na tela a mensagem: "Olá, `nome`,  você tem `idade` anos". Lembre-se: `nome` e `idade` são os valores inseridos pelo usuário.
+
+>💡  Dica: para imprimir mais de um valor na mesma linha, você pode usar essa sintaxe:
+```jsx
+console.log(valor1, valor2)
+``` */
+
+/* let nome1= ""
+let idade1 = ""
+console.log(typeof nome1, "e", typeof idade1) */
 
 // apareceu undefined para as duas variáveis
 // apareceu undefined por não ter atrbuído valores
