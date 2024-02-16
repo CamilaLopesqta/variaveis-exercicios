ad0a8f74e0d372c9806dbcf37633b5179ee1f931
Camila Lopes <camilalopesqta@gmail.com>
2024-02-16 17:58:16 -0300

    Exercícios do feed-back de variáveis

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,40 +1,27 @@
-/* ## Exercício 2
-
-Agora vamos criar 3 perguntas com valores de Sim ou Não.
-
-Siga as instruções abaixo:
-
-**a)** Crie um programa que faça 3 perguntas para o usuário **através do prompt**. Armazene a resposta de cada pergunta em uma variável.
-
-**b)** Imprima no console todas as respostas.
-
-**c)** Altere seu programa para que os textos das perguntas sejam armazenados em 3 variáveis diferentes. 
-
-**d)** Troque o texto do seu prompt pelas variáveis que contêm as perguntas.
-
-```
-//mandando a pergunta como texto direto
-const respostaA = prompt("Você está usando uma roupa azul hoje?");
-
-//mandando a pergunta como variável
-const perguntaA = "Você está usando uma roupa azul hoje?";
-const respostaA = prompt(perguntaA);
-
-```
-e) Altere o console para imprimir a pergunta e a resposta. 
-
-```
-Você está usando uma roupa azul hoje? - SIM
-```
- */
-
+//## Exercício 2
+//Agora vamos criar 3 perguntas com valores de Sim ou Não.
+//Siga as instruções abaixo:
+//**a)** Crie um programa que faça 3 perguntas para o usuário **através do prompt**. Armazene a resposta de cada pergunta em uma variável.
 //let pergunta1 = "Você está trabalhando hoje?"
 //let pergunta2 = "Você já almoçou?"
 //let pergunta3 = "Você já lavou a louça?"
 
+//**b)** Imprima no console todas as respostas.
 //let resposta1 = prompt(pergunta1)
 //let resposta2 = prompt(pergunta2)
 //let resposta3 = prompt(pergunta3)
+
+//**c)** Altere seu programa para que os textos das perguntas sejam armazenados em 3 variáveis diferentes. 
+
+//**d)** Troque o texto do seu prompt pelas variáveis que contêm as perguntas.
+
+//mandando a pergunta como texto direto
+//const respostaA = prompt("Você está usando uma roupa azul hoje?");
+//mandando a pergunta como variável
+//const perguntaA = "Você está usando uma roupa azul hoje?";
+//const respostaA = prompt(perguntaA);
+//e) Altere o console para imprimir a pergunta e a resposta. 
+//Você está usando uma roupa azul hoje? - SIM
 
 //console.log(resposta1, resposta2, resposta3)
 
@@ -56,4 +43,3 @@ let resposta3 = prompt(pergunta3)
 console.log(pergunta1, resposta1)
 console.log(pergunta2, resposta2)
 console.log(pergunta3, resposta3)
-
